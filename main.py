b8cf39225f40942f768cd39b8246026f6bfd6fdf
Michał <michal.kalicki@outlook.com>
2024-04-24 11:02:36 +0200

    Guessing game

--- main.py
+++ main.py
@@ -10,3 +10,21 @@ def user_input():
         print(f'{user_input} is not in ["too small", "too big", "you won"]')
     else:
         return user_input
+
+print("Think of a number  between 0 and 1000, and i will guess it in max. 10 tries.")
+print('Use "too big", "too small", "you won" to hint me :)')
+
+min = 0
+max = 1000
+
+while True:
+    guess = int((max - min) / 2) + min
+    print("I'm guessing: " + str(guess))
+    hint = user_input()
+    if hint == "you won":
+        print("I Won!")
+        break
+    elif hint == "too big":
+        max = guess
+    elif hint == "too small":
+        min = guess
